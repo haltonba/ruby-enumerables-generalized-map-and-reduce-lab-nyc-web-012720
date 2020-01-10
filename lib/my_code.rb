676a2a4219b82fdb2fe6e9b80f28bdc558ca51a3
Halton Bagley <haltonba94@gmail.com>
2020-01-10 18:22:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,14 @@
-# Your Code Here
+def map (array)
+  new_array = []
+  counter = 0
+  while counter < array.length do
+    new_array << yield(array[counter])
+    counter += 1
+  end
+  new_array
+end
+
+def reduce (array, starting_point = 0)
+  
+end
+
