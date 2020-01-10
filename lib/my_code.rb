28813a655b44bd335fa71b9effd4f6bb450ae7a0
Halton Bagley <haltonba94@gmail.com>
2020-01-10 19:03:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,8 +11,9 @@ end
 def reduce (array, starting_point = 0)
   counter = 0
   while counter < array.length do
-    
+    yield(starting_point, array[counter])
     counter += 1
   end
+  starting_point
 end
 
